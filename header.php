5bdb307e288dc3fba5f1e2bb7a749545d075df0e
leeannrhoden <31945397+leeannrhoden@users.noreply.github.com>
2017-11-15 21:16:34 -0800

    11/15

--- header.php
+++ header.php
@@ -12,7 +12,9 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
+
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<link href="https://fonts.googleapis.com/css?family=Poppins:400,500,700" rel="stylesheet">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<?php wp_head(); ?>
 </head>
--- template-parts/content-dropscroller.php
+++ template-parts/content-dropscroller.php
@@ -1,4 +1,6 @@
 <!-- Displaying Banner & Banner Text -->
+
+
 <div class="image_wrapper">
 
 	<?php
@@ -21,24 +23,78 @@
 
 <!-- Dsiplaying About -->
 
+<div >
+	
+	<h2 class="title">
+		<?php the_field('about_title') ?>
+	</h2>
 
+	<p class="content">
+		<?php the_field('purchase_drop') ?>
+	</p>
+</div>
 
 <!-- Displaying Step One -->
-
+<div class="gradient">
+	<h2>
+		<?php the_field('step_one_title') ?>
+	</h2>
+	<p>
+		<?php the_field('step_one') ?>
+	</p>
+	<img src="<?php the_field('step_one_image') ?>" alt="">
+</div>
 
  <!-- Displaying Step Two -->
-
+<div class=>
+		<h2>
+			<?php the_field('step_two_title') ?>
+		</h2>
+		<p>
+			<?php the_field('step_two') ?>
+		</p>
+		<img src="<?php the_field('step_two_image') ?>" alt="">
+</div>
 
  <!-- Displaying Step Three -->
-
+<div class="gradient">
+		<h2>
+			<?php the_field('step_three_title') ?>
+		</h2>
+		<p>
+			<?php the_field('step_three') ?>
+		</p>
+		<img src="<?php the_field('step_three_image') ?>" alt="">
+</div>
 
  <!-- Displaying Our Team Blurb -->
+<div >
+	
+	<h2 class="title">
+		<?php the_field('our_team_title') ?>
+	</h2>
+
+	<p class="content">
+		<?php the_field('our_team') ?>
+	</p>
+</div>
 
 
  <!-- Displaying Team Members -->
 
 
  <!-- Displaying Purchase Drop -->
+<div >
+	
+	<h2 class="title">
+		<?php the_field('purchase_drop_title'); ?>
+	</h2>
+
+	<p class="content">
+		<?php the_field('purchase_drop'); ?>
+	</p>
+
+</div>
 
 
  <!-- Displaying Content Form -->
